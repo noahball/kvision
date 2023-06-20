f45eac804fa45ad5b9db6ad666a18b898537f309
Noah Ball <noah@noahball.com>
2023-06-20 14:00:18 +1200

    Initial Commit

---
+++ kvision/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  kvision
+//
+//  Created by Noah Ball on 20/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ kvision/kvisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  kvisionApp.swift
+//  kvision
+//
+//  Created by Noah Ball on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct kvisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
